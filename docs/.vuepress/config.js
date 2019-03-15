213524880fdcf27f1610fea66bde42f1a7232ec8
vmartuniyk <volodimir.martuniyk@gmail.com>
2019-03-15 21:15:38 +0200

    Fist version blog on vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'Hello Vova',
+  description: 'Just playing around',
+  themeConfig: {
+    nav:[
+      {text:'Home',link:'/'},
+      {text:'Poland',link:'/poland/'},
+      {text:'Ukraine',link:'/ukraine/'},
+      {text:'Slovalia',link:'/slovakia'},
+      {text:'Blog',link:'/interviews/'},
+    ]
+  }
+}
